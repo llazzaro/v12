e0384170b1c08901170935e35e75f0c64c5708da
Leonardo Lazzaro <lazzaroleonardo@gmail.com>
2013-07-02 23:38:56 -0300

    new directory skeleton and setup. add gitignore

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'twisted',
+    ]
+
+setup(name='v12',
+      version='0.1',
+      description='v12: Asyc WWW',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Leonardo Lazzaro',
+      author_email='lazzaroleonardo@gmail.com',
+      url='www.lazzaroleonardo.com.ar',
+      keywords='www asynchronic',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="v12",
+      )
